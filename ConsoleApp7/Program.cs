e6938bf028e5fcc5beea14d9bafd8ce21639c5d8
ACER <ACER@DESKTOP-1BR1UTS>
2022-01-11 09:28:18 +0530

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Hemanth", "Rohith", "Surya",
+                       "Manoj", "Naveen", "Sowmya",
+                       "Bhavya", "Suman" };
+            //Using LINQ Queries
+            UsingLinq(names);
+        }
+
+        private static void UsingLinq(string[] names)
+        {
+            IEnumerable<string> query = from a in names
+                                        where a.Length == 5
+                                        orderby a
+                                        select a.ToUpper();
+            //select * from
+            foreach (string item in query)
+                Console.WriteLine(item);
+
+            Console.Read();
+
+        }
+    }
+}
